9c61c8b7bb45937391e0c298b8b12544742693d0
Aman Singh <amanksofficial@gmail.com>
2021-07-12 21:41:16 +0530

    added styling

---
+++ src/components/SendMessage.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { db, auth } from "../firebase";
+import { Input, Button } from "@material-ui/core";
+import firebase from "firebase";
+
+function SendMessage({ scroll }) {
+    const [message, setMessage] = useState("");
+
+    async function sendMessage(e) {
+        e.preventDefault();
+        const { uid, photoURL } = auth.currentUser;
+
+        await db.collection("messages").add({
+            text: message,
+            photoURL,
+            uid,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+
+        setMessage("");
+        scroll.current.scrollIntoView({ behavior: "smooth" });
+    }
+    return (
+        <div>
+            <form onSubmit={sendMessage}>
+                <div className="sendMsg">
+                    <Input
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="Message..."
+                        type="text"
+                        style={{
+                            width: "78%",
+                            fontSize: "15px",
+                            fontWeight: "550",
+                            marginLeft: "5px",
+                            marginBottom: "-3px",
+                        }}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{
+                            width: "18%",
+                            fontSize: "15px",
+                            fontWeight: "550",
+                            margin: "4px 5% -13px 5%",
+                            maxWidth: "200px",
+                        }}
+                        type="submit"
+                    >
+                        Send
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default SendMessage;
--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -3,8 +3,27 @@ import { auth } from "../firebase";
 import { Button } from "@material-ui/core";
 function SignOut() {
     return (
-        <div>
-            <Button onClick={() => auth.signOut()}>Log out</Button>
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                position: "fixed",
+                width: "100%",
+                backgroundColor: "#FAFAFA",
+                top: 0,
+                borderBottom: "solid 1px lightgray",
+                zIndex: "10",
+                padding: "0.5rem 1rem",
+            }}
+        >
+            <Button
+                variant="outlined"
+                size="medium"
+                color="secondary"
+                onClick={() => auth.signOut()}
+            >
+                Log out
+            </Button>
         </div>
     );
 }
